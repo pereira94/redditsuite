9f6c3fb72c51e37d2dff10625e8605995309b15f
Pedro Pereira <pedropereira@Pedros-MBP.attlocal.net>
2021-02-06 16:21:40 -0800

    Update main R

--- main.R
+++ main.R
@@ -8,3 +8,18 @@ pg_connection <- dbConnect(
   password = Sys.getenv("POSTGRES_PASSWORD"),
   dbname = Sys.getenv("POSTGRES_DB")
 )
+
+dbCreateTable(pg_connection, 'mtcars', fields = mtcars)
+
+dbListTables(pg_connection)
+
+dbAppendTable(pg_connection, name = 'mtcars', mtcars)
+
+mtcars_conn <-
+  tbl(pg_connection, in_schema('public', 'mtcars'))
+
+mtcars_conn %>%
+  mutate(
+    carb = carb * 2
+  )
+
