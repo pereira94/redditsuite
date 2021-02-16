12e364b2e9052be4d0fd79ba49f5b03c7f0224c7
pereira94 <pedropereira0426@gmail.com>
2021-02-15 16:43:39 -0800

    Added conn to database

--- main.R
+++ main.R
@@ -1,25 +1,5 @@
 library(redditsuite)
 
-pg_connection <- dbConnect(
-  drv = Postgres(),
-  host = 'localhost',
-  port = 5432,
-  user = Sys.getenv("POSTGRES_USER"),
-  password = Sys.getenv("POSTGRES_PASSWORD"),
-  dbname = Sys.getenv("POSTGRES_DB")
-)
+con <- connection_postgres()
 
-dbCreateTable(pg_connection, 'mtcars', fields = mtcars)
-
-dbListTables(pg_connection)
-
-dbAppendTable(pg_connection, name = 'mtcars', mtcars)
-
-mtcars_conn <-
-  tbl(pg_connection, in_schema('public', 'mtcars'))
-
-mtcars_conn %>%
-  mutate(
-    carb = carb * 2
-  )
 
